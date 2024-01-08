7270e9a49d51295720706e72641389b18f50415e
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-01-08 06:03:59 +0100

    secondcomit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.sql.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+     String sql = "SELECT TESTVAL FROM pgtestManual WHERE id =12";
+     String url = "jdbc:postgresql://localhost:5432/postgres";
+     String username = "postgres";
+     String password= "0000";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
+       ResultSet rs = st.executeQuery(sql);
+       rs.next();
+       String res = rs.getString(1);
+        System.out.println(res);
+        con.close();
     }
 }
